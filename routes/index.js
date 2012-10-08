620899be3821507aa233ea9b556efcde8b0f2faf
Jack Wink <jackwink@umich.edu>
2012-10-08 00:59:58 -0400

    typography changes and about page added

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ exports.about = function(req, res){
 	i = Math.floor(Math.random()*verbs.length);
 	//need page for this
   res.render('about', { title: 'Hack Michigan',
-  	info: "Hack Michigan is Michigan Hackers second hackathon and aims to be the biggest student run hackathon ever held at the University of Michigan. The theme of this hackathon is “impact”. As such, we want each and every team to build something that has an impact on real people. Hack Michigan is scheduled for November 3rd - 4th, and will be held in North Quad. The event will be 28 hours long with 24 hours devoted to hacking." ,
+  	info: "" ,
   	verb: verbs[i]
   });
 };
